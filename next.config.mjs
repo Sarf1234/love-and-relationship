b5d78abd175ccf91aaed6f11ccaa2f89b9bda025
Sarf1234 <sarfarajshekh69@gmail.com>
2025-11-29 23:18:59 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+
+  experimental: {
+    forceSwcTransforms: true,
+    optimizePackageImports: [],
+  },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/images/**",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/dsc5aznps/image/upload/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dsc5aznps/image/upload/**",
       },
     ],
   },
